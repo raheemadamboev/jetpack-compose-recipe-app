a8efa20131179199c86b10567c56610b251b4ed1
Raheem <raheemideadev@gmail.com>
2021-04-14 22:34:06 +0500

    build api

---
+++ app/src/main/java/com/example/composerecipeapp/api/RecipeApi.kt
@@ -0,0 +1,23 @@
+package com.example.composerecipeapp.api
+
+import com.example.composerecipeapp.model.RecipeNetworkModel
+import com.example.composerecipeapp.model.SearchResponseModel
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Query
+
+interface RecipeApi {
+
+    @GET("search")
+    suspend fun search(
+        @Header("Authorization") token: String,
+        @Query("page") page: Int,
+        @Query("query") query: String
+    ): SearchResponseModel
+
+    @GET("get")
+    suspend fun get(
+        @Header("Authorization") token: String,
+        @Query("id") id: Int
+    ): RecipeNetworkModel
+}
---
+++ app/src/main/java/com/example/composerecipeapp/model/SearchResponseModel.kt
@@ -0,0 +1,12 @@
+package com.example.composerecipeapp.model
+
+import com.google.gson.annotations.SerializedName
+
+data class SearchResponseModel(
+
+    @SerializedName("count")
+    val count: Int,
+
+    @SerializedName("results")
+    val recipes: List<RecipeNetworkModel>
+)

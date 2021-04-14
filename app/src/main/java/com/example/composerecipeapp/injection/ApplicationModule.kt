36710bf24915dfc1777c64afbb06763021a34399
Raheem <raheemideadev@gmail.com>
2021-04-14 23:30:34 +0500

    injection setup

---
+++ app/src/main/java/com/example/composerecipeapp/injection/ApplicationModule.kt
@@ -0,0 +1,22 @@
+package com.example.composerecipeapp.injection
+
+import android.content.Context
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ApplicationModule {
+
+    @Singleton
+    @Provides
+    fun provideApplication(@ApplicationContext app: Context): App {
+        return app as App
+    }
+
+
+}

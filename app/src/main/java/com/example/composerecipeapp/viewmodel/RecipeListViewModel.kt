f6dce40e4de179d0bddeda6a2c2472bebb768241
Raheem <raheemideadev@gmail.com>
2021-04-15 20:19:06 +0500

    mutable state

--- app/src/main/java/com/example/composerecipeapp/viewmodel/RecipeListViewModel.kt
+++ app/src/main/java/com/example/composerecipeapp/viewmodel/RecipeListViewModel.kt
@@ -1,6 +1,9 @@
 package com.example.composerecipeapp.viewmodel
 
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import com.example.composerecipeapp.model.RecipeModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
@@ -8,4 +11,7 @@ import javax.inject.Inject
 class RecipeListViewModel @Inject constructor(
     private val repository: RecipeRepository,
 ) : ViewModel() {
+
+    val recipes: MutableState<List<RecipeModel>> = mutableStateOf(listOf())
+
 }
